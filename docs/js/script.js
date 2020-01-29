dab6fdc337976a8f15f14533d65819a173b6e4fd
Valeriy <goldaboss@yandex.ru>
2020-01-30 00:27:46 +0300

    1

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,14 +1,1 @@
 'use strict';
-
-var mySwiper = new Swiper('.js-slider', {
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination'
-  },
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev'
-  },
-  loop: true
-});
