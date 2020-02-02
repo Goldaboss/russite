0bf2553318bdbfa5c6364583046a5df4bb1cc11f
Valeriy <goldaboss@yandex.ru>
2020-02-02 17:33:37 +0300

    active btn

--- app/script.js
+++ app/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+$(document).ready(function () {
+    $('.js-btn-start').on('click', function () {
+        let self = this;
+        setTimeout(function () {
+            $(self).parent().remove();
+        }, 5000)
+
+    })
+});
--- docs/js/script.js
+++ docs/js/script.js
@@ -1,2 +1,10 @@
 'use strict';
 
+$(document).ready(function () {
+  $('.js-btn-start').on('click', function () {
+    var self = this;
+    setTimeout(function () {
+      $(self).parent().remove();
+    }, 5000);
+  });
+});
